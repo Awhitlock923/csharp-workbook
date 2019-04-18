18946d2d587e31cbcd7ad5238e3e7e9ea1c0b56c
Austin Whitlock <awhitlock923@gmail.com>
2019-04-18 01:02:43 -0500

    n

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -18,9 +18,9 @@ namespace TicTacToe
             {
                 DrawBoard();
                 GetInput();
-
+                playerTurn = (playerTurn == "x") ? "O" : "X";
             } while (!CheckForWin() && !CheckForTie());
-
+            
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
@@ -37,19 +37,42 @@ namespace TicTacToe
         public static void PlaceMark(int row, int column)
         {
         // your code goes here
+
+        //grabs the position from user input and places the x or o in the correct space
+            GetInput();
+
+            // board = board[row][column];
+        
         }
 
         public static bool CheckForWin()
         {
             // your code goes here
+            HorizontalWin();
+            DiagonalWin();
+            VerticalWin();
 
-            return false;
+            if (HorizontalWin = true)
+            {
+                return true;
+            }else if ( VerticalWin = true){
+                return true;
+            }else if (DiagonalWin = true){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         public static bool CheckForTie()
         {
             // your code goes here
 
+            // get input can't take anymore input return true
+            
             return false;
         }
         
@@ -57,21 +80,57 @@ namespace TicTacToe
         {
         // your code goes here
 
-        return false;
+        // 0: 0,1,2
+        // 1: 0,1,2
+        // 2: 0,1,2
+
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][2]){
+            return true;
+        }else if(board[1][0] == board[1][1] && board[1][1] == board[1][2]){
+            return true;
+        }else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]){
+            return true;
+        }else{
+            return false;
+        }
+        
+
+        
         }
 
         public static bool VerticalWin()
         {
             // your code goes here
 
+            // 0:0 , 1:0 , 2:0
+            // 0:1 , 1:1 , 2:1
+            // 0:2 , 1:2 , 2:2
+
+            if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+            return true;
+        }else if(board[0][01] == board[1][1] && board[1][1] == board[2][1]){
+            return true;
+        }else if(board[0][2] == board[2][1] && board[1][2] == board[2][2]){
+            return true;
+        }else{
             return false;
+        }
         }
 
         public static bool DiagonalWin()
         {
             // your code goes here
 
+            // 0:0 , 1:1 , 2:2
+            // 0:2 , 1:1 , 2:0
+
+            if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            return true;
+        }else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+            return true;
+        }else{
             return false;
+        }
         }
 
         public static void DrawBoard()
