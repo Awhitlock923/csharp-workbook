f779bf52eff020c2ad7174f24b99846f69af9166
Austin Whitlock <awhitlock923@gmail.com>
2019-04-25 20:11:01 -0500

    n

---
+++ Exercice4-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercice4_2 {
+    class Program {
+        static void Main (string[] args) {
+
+            List<Person> people = new List<Person> ();
+
+            for (int i = 0; i < 3; i++) {
+                Person x = new Person ();
+
+                Console.WriteLine ("Please Type your first name.");
+                x.fName = Console.ReadLine ();
+
+                Console.WriteLine ("Please enter last name.");
+                x.lName = Console.ReadLine ();
+
+                Console.WriteLine ("Enter the date of birth.");
+                x.dob = Console.ReadLine ();
+
+                people.Add (x);
+            }
+
+            for (int i = 0; i < people.Count; i++) {
+                Person w = people[i];
+                string fullName = Person.staticFullName (w.fName, w.lName);
+                Console.WriteLine ("Hello " + fullName + " " + " you were born on " + w.dob);
+
+            }
+
+        }
+
+        
+    }
+
+    class Person {
+        public string fName;
+        public string lName;
+        public string dob;
+
+        public static string staticFullName (string f, string l) {
+            string full = f + " " + l;
+            return full;
+        }
+
+        public string nonStaticFullName(){
+            return this.fName+" "+this.lName;
+        }
+    }
+}
