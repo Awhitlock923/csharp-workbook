de81fa8a740cb9335553aaca368a25cd0c5cdf1f
Austin Whitlock <awhitlock923@gmail.com>
2019-04-25 20:45:23 -0500

    n

--- Gradebook/Program.cs
+++ Gradebook/Program.cs
@@ -7,30 +7,20 @@ namespace RandomList
     {
         static void Main(string[] args) {
         
+            string name;
+            int grade;
 
-            // write a program that asks the user for 5 integers.
-            // handle the user giving you non-integer input.
-
-            Dictionary<String,String> studentList = new Dictionary<String,String>();
-            Console.WriteLine("Students first");
-            studentList.Add(Console.ReadLine(),Console.ReadLine());
-            Console.WriteLine(studentList);
-
-            studentList.Add("","");
-            studentList.Add("", "");
+            List<string[]> student = new List<string[]>();
+            List<int[]> grades = new List<int[]>();
+            
+            for(int i = 0; i<5; i++)
+            {
+                
 
 
-
-            // looping through the keys of my dictionary
-            foreach(String student in studentList.Keys) {
-                Console.WriteLine(student);
-
-                // for every key, i want the value
-                String grades = studentList[student];
-                Console.WriteLine("The grade of {0} is {1}", student, grades);
             }
-
-            // looping through the entries in my dictionary
+            
+        
             
         }    
 
