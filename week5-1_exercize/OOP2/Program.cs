539beecaa426902c392bd5a6df40747ca137605f
Austin Whitlock <awhitlock923@gmail.com>
2019-05-02 18:25:26 -0500

    n

--- week5-1_exercize/OOP2/Program.cs
+++ week5-1_exercize/OOP2/Program.cs
@@ -6,17 +6,49 @@ namespace OOP2
     {
         static void Main(string[] args)
         {
+            Passengers x1 = new Passengers("John Doe", "Jane Doe");
+
+            Passengers x2 = new Passengers("Mark Freeman", "Rose Freeman");
+
+            Passengers x3 = new Passengers("Bob Smith", "Julia Smith");
+
             Car c1 = new Car("Black", "VW", "Jetta");
             // Console.WriteLine("c1 = {0}", c1.ToString());
 
             Car c2 = new Car("Blue", "Mazda", "3");
             // Console.WriteLine("c2 = {0}", c2.ToString());
 
+            Car c3 = new Car("Yellow", "Cheverle", "Camaro");
+
+            c1.seats(x1, 1);
+            c2.seats(x2, 1);
+            c3.seats(x3, 1);
+
             ParkingGarage p1 = new ParkingGarage(7);
             p1.parkCar(c1, 1);
-            p1.towCar(1);
-            p1.parkCar(c2, 1);
+            // p1.towCar(1);
+            p1.parkCar(c2, 2);
+            p1.parkCar(c3, 3);
             
+        }
+    }
+
+
+
+    public class Passengers
+    {
+        public string nameOne;
+        public string nameTwo;
+
+        public Passengers(string initialNameOne, string initialNameTwo)
+        {
+            this.nameOne = initialNameOne;
+            this.nameTwo = initialNameTwo;
+        }
+
+        public String ToString(){
+            string stringRepresentation = string.Format("{0} {1}", this.nameOne, this.nameTwo);
+            return stringRepresentation;
         }
     }
 
@@ -36,7 +68,9 @@ namespace OOP2
         public string plateNumber;
         public int year;
 
-        public Car(String initialColor, string initialMake, string initialModel)
+        private Passengers[]  availableSeats;
+
+        public Car(string initialColor, string initialMake, string initialModel)
         {
             this.color = initialColor;
             this.make = initialMake;
@@ -46,6 +80,32 @@ namespace OOP2
         public String ToString(){
             string stringRepresentation = string.Format("{0} {1} {2}", this.color, this.make, this.model);
             return stringRepresentation;
+        }
+
+        public Car(int desiredNumberOfSeats)
+        {
+            availableSeats = new Passengers[desiredNumberOfSeats];
+        }
+
+        public bool isSeatAvailable(int seatNumber){
+            Passengers thePeopleInTheSeats = availableSeats[seatNumber];
+            if(availableSeats[seatNumber] == null){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public void seats(Passengers people, int seats)
+        {
+            bool isSeatEmpty = isSeatAvailable(seats);
+            if(isSeatEmpty == true)
+            {
+            // bad parking garage did not actually park my car ....
+            this.availableSeats[seats]= people;
+            }else{
+                throw new Exception("Slot "+seats+" is taken!");
+            }
         }
 
     }
